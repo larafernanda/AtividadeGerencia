10254fd81a47ef9f5361e3aa18c13e776136527e
Lara Fernanda <larafvitoriano@gmail.com>
2020-05-10 00:28:05 -0300

    Atividade Finalizada

--- AtividadeGitMerging/src/util/Util.java
+++ AtividadeGitMerging/src/util/Util.java
@@ -5,13 +5,38 @@
  */
 package util;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Lara
  */
 public class Util {
+    
+    
     //Método IMC (Lara) - Dev1
     
-    
+     public void calculoIMC(int imc, int peso, int altura) { 
+        imc = peso / altura * altura;
+    }
+     
     //Método Conversão (Alicia) - Dev2
+    	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+		double Celsius, Fahrenheit; 
+
+		System.out.print("Conversor de temperatura: Graus Celsios -> Fahrenheit\n\n");
+		
+		
+		System.out.print("Digite a temperatura em Celsius: ");
+		Celsius = entrada.nextDouble();
+		
+		Fahrenheit = ( 9 * Celsius + 160 ) / 5;
+
+		System.out.print("\n A medida convertida é " + Fahrenheit + "ºF\n");
+		
+	}
+
 }
+
